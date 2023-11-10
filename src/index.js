460d6aa288b1d65e76a0eb314d4f9bd5a935caea
Ryan Nam <alfofh7060@gmail.com>
2023-11-10 20:45:08 +1100

    Install TailwindCSS

--- src/index.js
+++ src/index.js
@@ -4,10 +4,43 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import NotFound from './pages/NotFound';
+import Contacts from './components/Contacts';
+import ContactDetail from './pages/ContactDetail';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: true,
+        element: <Contacts />,
+      },
+      {
+        path: '/contacts',
+        element: <Contacts />,
+      },
+      {
+        path: '/contacts/:keyword',
+        element: <Contacts />,
+      },
+      {
+        path: '/contacts/watch/:id',
+        element: <ContactDetail />,
+      },
+    ],
+  },
+  // Any additional routes can be added here
+]);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />;
   </React.StrictMode>
 );
 
---
+++ src/pages/ContactDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function ContactDetail() {
+    return (
+        <div>
+            Contact Detail page
+            will show the detail of that
+        </div>
+    );
+}
+
