4518937814c815d62fd673094a062715160cb1a9
Ryan Nam <alfofh7060@gmail.com>
2023-11-12 00:46:36 +1100

    Add google api env

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-export default function ContactCard({contact}) {
-    return (
-        <div>
-            {contact.name}
-        </div>
-    );
+
+export default function ContactCard({ contact }) {
+
+  const { name, username, email, address, phone, website, company } = contact;
+  const navigate = useNavigate();
+  return (
+    <li onClick={()=>{navigate(`contacts/watch/${contact.id}`, {state: {contact}})}}>
+      <div>
+        <p>{name}</p>
+        <p>{username}</p>
+        <p>{email}</p>
+        <p>{phone}</p>
+        <p>{website}</p>
+      </div>
+      <br />
+    </li>
+  );
 }
-
--- src/pages/ContactDetail.jsx
+++ src/pages/ContactDetail.jsx
@@ -1,11 +1,65 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 export default function ContactDetail() {
-    return (
-        <div>
-            Contact Detail page
-            will show the detail of that
-        </div>
-    );
-}
+  const {
+    state: { contact },
+  } = useLocation();
+  // console.log(state);
+  console.log(contact);
+  // Replace 'YOUR_API_KEY' with your actual Google Maps API key
+  const googleMapsApiKey = process.env.REACT_APP_CONTACT_GOOGLEMAP_API_KEY;
 
+  // Function to create the Google Maps Embed URL using contact's geo data
+  const createGoogleMapsUrl = (geo) => {
+    const baseUrl = 'https://www.google.com/maps/embed/v1/view';
+    return `${baseUrl}?key=${googleMapsApiKey}&center=${geo.lat},${geo.lng}&zoom=18&maptype=satellite`;
+  };
+
+  return (
+    <article>
+      <div>
+        <h1>Contact Detail Page</h1>
+
+        <h2>
+          {contact.name} ({contact.username})
+        </h2>
+        <p>Email: {contact.email}</p>
+        <p>
+          Address: {contact.address.street}, {contact.address.suite},{' '}
+          {contact.address.city}, {contact.address.zipcode}
+        </p>
+        <p>Phone: {contact.phone}</p>
+        <p>
+          Website:{' '}
+          <a
+            href={`http://${contact.website}`}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            {contact.website}
+          </a>
+        </p>
+        <p>Company: {contact.company.name}</p>
+        <p>Catchphrase: {contact.company.catchPhrase}</p>
+        <p>Business Strategy: {contact.company.bs}</p>
+
+        {/* ... other contact details */}
+        {contact.address.geo && (
+          <div>
+            <h4>Location on Map</h4>
+            <iframe
+              width='600'
+              height='450'
+              style={{ border: 0 }}
+              loading='lazy'
+              allowFullScreen
+              referrerPolicy='no-referrer-when-downgrade'
+              src={createGoogleMapsUrl(contact.address.geo)}
+            ></iframe>
+          </div>
+        )}
+      </div>
+    </article>
+  );
+}
