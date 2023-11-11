e18897bf3bbaac28c7fa4f03ef2edf0d5c67d2c8
Ryan Nam <alfofh7060@gmail.com>
2023-11-11 22:47:58 +1100

    Add React Query

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import './App.css';
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import SearchHeader from './components/SearchHeader';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 function App() {
+  
+  const queryClient = new QueryClient();
   return (
     <>
       <Navbar />
       <SearchHeader />
-      <Outlet />
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+      </QueryClientProvider>
     </>
   );
 }
